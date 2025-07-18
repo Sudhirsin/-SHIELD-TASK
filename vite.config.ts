f6b4c0b8da8adcfa80599ee14e63e626f0d388de
Sudhir Singh <56826370+Sudhirsin@users.noreply.github.com>
2025-07-18 10:09:04 +0000

    Fixed codesandbx issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "localhost",
+      ".csb.app",
+      ".codesandbox.io",
+      "gjt4d6-5173.csb.app",
+    ],
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "localhost",
+      ".csb.app",
+      ".codesandbox.io",
+      "gjt4d6-5173.csb.app",
+    ],
+  },
+});
